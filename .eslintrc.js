7c1b6d2966e1e9a8bac3da6d711a058a85b1502b
Jennifer Winer <jeniwiner@gmail.com>
2017-03-03 10:06:04 -0700

    added eslint files and cleaned up img folder

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'ryansobol/browser',
+    'ryansobol/jquery',
+    'ryansobol/es6'
+  ],
+  rules: {
+    // "semi": "off",
+    "prefer-const": "off",
+    "no-implicit-globals": "off",
+    "strict": "off",
+    "quotes": "off",
+    "no-unused-vars": "off",
+    "no-else-return": "off",
+    "no-multiple-empty-lines": "off",
+    "lines-around-comment": "off",
+    "func-style": "off",
+    "no-console": "off",
+    "dot-notation": "off",
+    "arrow-parens": "off",
+    "object-shorthand": "off",
+    "prefer-arrow-callback": "off",
+    "no-var": "off",
+    "no-undefined": "off",
+    "no-unused-expressions": "off"
+  }
+};
